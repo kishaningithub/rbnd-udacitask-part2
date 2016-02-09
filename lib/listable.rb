21a29dc82e8a786d0f44cf3c31280bb5d75e6b9f
Kishan B <b.kishan.n@gmail.com>
2016-02-09 03:54:41 +0000

    feat(format_date,format_priority): Implement and mixin format_date and format_priority in todo items

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,18 @@ module Listable
   def format_description(description)
       "#{description}".ljust(30)
   end
+  def format_date(date1, date2, msgIfNull)
+      date = ""
+      date << date1.strftime("%D") if date1
+      date << " -- " + date2.strftime("%D") if date2
+      date = msgIfNull if date.empty?
+      date
+  end
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
